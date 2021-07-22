810f61cdbb31df6002e082fe361f0e2afe27fd6b
ldauparas <luke.dauparas@gmail.com>
2021-07-21 19:25:52 -0500

    working on code to extract images for celebs that have them on their page

--- scripts/scrape-data.js
+++ scripts/scrape-data.js
@@ -1,13 +1,40 @@
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const fetch = require('isomorphic-fetch');
+const base_url = 'https://en.wikipedia.org';
 
 (async () => {
-    const response = await fetch('https://en.wikipedia.org/wiki/List_of_American_film_actresses');
-    const text = await response.text();
-    const dom = await new JSDOM(text);
-    dom.window.document.querySelectorAll('li').forEach(a => {
-        const plainText = a.textContent;
-        if (plainText.match('born')) {console.log(plainText)};
+
+    var response = await fetch(base_url + '/wiki/List_of_American_film_actresses');
+    var text = await response.text();
+    var dom = await new JSDOM(text);
+    const celebArr = [];
+
+    dom.window.document.querySelectorAll('li').forEach(listElement => {
+        const plainText = listElement.textContent;
+        if (plainText.match('born')) {
+            listElement.querySelectorAll('a').forEach(linkElement => {
+                celebArr.push(linkElement.getAttributeNode('href').textContent)
+            })
+        };
     });
+    
+    for (let celeb_url in celebArr.slice(15,20)) {
+        
+        var response = await fetch(base_url + celebArr[celeb_url]);
+        var text = await response.text();
+        var dom = await new JSDOM(text);
+        /*
+        const tableArr = dom.window.document.querySelectorAll('table').forEach(tableElement => {
+            console.log(tableElement.querySelectorAll('img').forEach(a => {
+                console.log(a.getAttributeNode('src').textContent);
+            }));
+        })
+        */
+        const tableArr = dom.window.document.querySelectorAll('img').forEach(a => {
+            console.log(a.getAttributeNode('src').textContent);
+        })
+        
+    }
+    
 })();
